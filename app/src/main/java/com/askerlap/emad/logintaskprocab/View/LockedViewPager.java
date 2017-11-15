eab318930869336e161974615c36f29c9dec8deb
emad3020 <askerasker2016@gmail.com>
2017-11-15 19:26:45 +0200

    create custom viewer pager without swiping

---
+++ app/src/main/java/com/askerlap/emad/logintaskprocab/View/LockedViewPager.java
@@ -0,0 +1,70 @@
+package com.askerlap.emad.logintaskprocab.View;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by emad on 11/15/17.
+ */
+
+public class LockedViewPager extends ViewPager {
+    private boolean swipeable;
+
+
+
+    public LockedViewPager(Context context) {
+
+        super(context);
+
+    }
+
+
+    public LockedViewPager(Context context, AttributeSet attrs) {
+
+        super(context, attrs);
+
+        this.swipeable = true;
+
+    }
+
+    @Override
+
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (this.swipeable) {
+
+            return super.onTouchEvent(event);
+
+        }
+
+
+        return false;
+
+    }
+
+
+    @Override
+
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+
+        if (this.swipeable) {
+
+            return super.onInterceptTouchEvent(event);
+
+        }
+
+
+        return false;
+
+    }
+
+
+    public void setSwipeable(boolean swipeable) {
+
+        this.swipeable = swipeable;
+
+    }
+
+}
