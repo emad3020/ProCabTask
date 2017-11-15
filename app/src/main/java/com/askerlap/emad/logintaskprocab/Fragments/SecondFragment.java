b305778211eea1afb0dbe878b19b059f5c47e736
emad3020 <askerasker2016@gmail.com>
2017-11-15 20:07:58 +0200

    adding option menu to the fragment

--- app/src/main/java/com/askerlap/emad/logintaskprocab/Fragments/SecondFragment.java
+++ app/src/main/java/com/askerlap/emad/logintaskprocab/Fragments/SecondFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -24,8 +27,13 @@ public class  SecondFragment extends Fragment {
         return secondFragment;
     }
 
-    //------------- inflating the layout to the fragment --------//
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
 
+    //------------- inflating the layout to the fragment --------//
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -33,5 +41,17 @@ public class  SecondFragment extends Fragment {
         boolean shouldAttachToParentImmediately = false;
         View fragmentView = inflater.inflate(LayoutToInflate,container,shouldAttachToParentImmediately);
         return fragmentView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.register_menu,menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int selectedItemId = item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 }
